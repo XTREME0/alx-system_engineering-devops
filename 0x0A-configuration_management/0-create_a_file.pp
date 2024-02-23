cef2d9ffa305009c4356c0225502d05063fc0c00
xtreme <anastaai94@gmail.com>
2024-02-23 17:41:38 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,12 @@
+file { '/tmp':
+  ensure => 'directory',
+  mode   => '1777',
+}
+
 file { 'school':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
-}
-
-file { '/tmp':
-  ensure => 'directory',
-  mode   => '1777',
 }
