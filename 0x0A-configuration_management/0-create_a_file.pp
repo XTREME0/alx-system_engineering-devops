48bff2765952aae431a5a78e992abeef1788a24f
xtreme <anastaai94@gmail.com>
2024-02-23 17:33:55 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { 'school':
-  path    => '/tmp/'
-  mode    => '0744'
-  owner   => 'www-data'
-  group   => 'www-data'
-  content => 'I love Puppet'
+  path    => '/tmp/',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
