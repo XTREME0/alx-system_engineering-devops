6af8951910e2a5f9a743da4276c6c14ae8fc3804
xtreme <anastaai94@gmail.com>
2024-03-25 20:43:44 +0000

    task 2

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""get todo list using rest api export to json"""
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+    id = sys.argv[1]
+    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/')
+    resp = resp.json()
+    user = resp.get("username")
+    resp = requests.get('https://jsonplaceholder.typicode.com'
+                        f'/users/{id}/todos/')
+    resp = resp.json()
+    with open(f'{id}.json', 'a') as file:
+        for task in resp:
+            json.dump({id: [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user
+                }, file)
