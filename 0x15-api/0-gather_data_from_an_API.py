403bb7c39dbecedcc97b342f974dd86ff209faf3
xtreme <anastaai94@gmail.com>
2024-03-25 14:11:36 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """get todo list using rest api"""
-import json
-import requests
-import sys
-id = sys.argv[1]
-tasks = []
-resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/')
-resp = resp.json()
-name = resp.get("name")
-resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos/')
-resp = resp.json()
-total = 0
-done = 0
-for task in resp:
-    total += 1
-    if (task.get("completed")):
-        tasks.append(task.get("title"))
-        done += 1
-print(f'Employee {name} is done with tasks({done}/{total}):')
-for task in tasks:
-    print(f'\t {task}')
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+    id = sys.argv[1]
+    tasks = []
+    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/')
+    resp = resp.json()
+    name = resp.get("name")
+    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos/')
+    resp = resp.json()
+    total = 0
+    done = 0
+    for task in resp:
+        total += 1
+        if (task.get("completed")):
+            tasks.append(task.get("title"))
+            done += 1
+    print(f'Employee {name} is done with tasks({done}/{total}):')
+    for task in tasks:
+        print(f'\t {task}')
