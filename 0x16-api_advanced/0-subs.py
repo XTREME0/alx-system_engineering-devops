09d4f1d93a5b55e17f29577240819f7550679ae3
xtreme <anastaai94@gmail.com>
2024-04-09 18:24:30 +0000

    fixed task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,9 @@ def number_of_subscribers(subreddit):
 
     endpoint = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    r = requests.get(endpoint)
+    r = requests.get(endpoint, allow_redirects=False)
+    while r.status_code == 429:
+        r = requests.get(endpoint, allow_redirects=False)
     if r.status_code == 200 and 'subscribers' in r.json().get('data'):
         return r.json().get('data').get('subscribers')
     return 0
